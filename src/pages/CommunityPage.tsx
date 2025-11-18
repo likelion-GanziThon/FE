9be47547849eebeea88d3f6be032dd0c10035db3
Dessert99 <lsjh1234@naver.com>
2025-11-18 14:19:24 +0900

    feat: 커뮤니티 페이지 검색 폼 구현
    #11

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,3 +1,57 @@
+import { FieldSet } from '@/components/ui/field';
+
+import { FormProvider, useForm, type SubmitHandler } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import SearchTypeField from '@/components/SearchTypeField';
+import SearchTextField from '@/components/SearchTextField';
+import RegionFieldGroup from '@/components/RegionFieldGroup';
+
+// 폼 타입
+export type CommunityFormValues = {
+  type: 'title' | 'content';
+  searchText: string;
+  region_1: string;
+  region_2: string;
+};
+
 export default function CommunityPage() {
-  return <div>커뮤니티 페이지</div>;
+  // 커뮤니티 검색 폼의 상태/유효성 관리 훅 생성
+  const communityForm = useForm<CommunityFormValues>({
+    defaultValues: {
+      type: 'title',
+      searchText: '',
+      region_1: '',
+      region_2: '',
+    },
+    mode: 'onSubmit',
+  });
+
+  // useForm객체 메서드 구조 분해
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = communityForm;
+
+  // 검색 폼 제출
+  const onSubmit: SubmitHandler<CommunityFormValues> = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className='px-10'>
+      <FormProvider {...communityForm}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FieldSet disabled={isSubmitting}>
+            <div className='flex gap-4'>
+              <SearchTypeField />
+              <SearchTextField />
+            </div>
+            <RegionFieldGroup />
+            <Button type='submit'>검색하기</Button>
+          </FieldSet>
+        </form>
+      </FormProvider>
+    </div>
+  );
 }
