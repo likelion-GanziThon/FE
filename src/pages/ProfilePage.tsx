7cadf2996e0569adb5c8af155953f08e8f41fef4
Dessert99 <lsjh1234@naver.com>
2025-11-20 22:11:11 +0900

    feat(profile): 프로필 페이지 api 연동 및 데이터 바인딩

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,28 +1,41 @@
+import GlobalLoader from '@/components/common/GlobalLoader';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import { useGetMe } from '@/hooks/queries/useProfile';
+
 import { Link } from 'react-router';
 
 export default function ProfilePage() {
-  const userId: number = 12;
+  const { data: user, isPending } = useGetMe();
+
+  if (isPending) return <GlobalLoader />;
   return (
     <div className='flex flex-col gap-10 p-8'>
       <div className='flex items-center gap-14'>
         <Avatar className='size-25'>
-          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarImage
+            src={
+              user?.profileImageUrl.startsWith('http')
+                ? user.profileImageUrl
+                : `${import.meta.env.VITE_API_URL}${user?.profileImageUrl}`
+            }
+          />
           <AvatarFallback>profile</AvatarFallback>
         </Avatar>
-        <span className='flex text-2xl'>닉네임</span>
+        <span className='flex text-2xl'>{user?.nickname}</span>
       </div>
       <div className='flex flex-col gap-6'>
         <div className='flex items-center'>
           <span className='w-32 text-xl'>희망 거주 지역</span>
           <div className='flex-1 rounded-2xl border bg-gray-200 py-2 text-center'>
-            서울 동대문구
+            {user?.desiredArea || '희망 거주 지역을 추가해주세요!'}
           </div>
         </div>
         <div className='flex items-center'>
           <span className='w-32 text-xl'>희망 입주 시기</span>
-          <div className='flex-1 rounded-2xl border bg-gray-200 py-2 text-center'>2025.12.21</div>
+          <div className='flex-1 rounded-2xl border bg-gray-200 py-2 text-center'>
+            {user?.desiredMoveInDate || '희망 입주 시기를 입력해주세요!'}
+          </div>
         </div>
       </div>
       <div>
@@ -30,13 +43,12 @@ export default function ProfilePage() {
         <p
           //whitespace-pre-line :줄바꿈 문자 기준으로 실제 줄이 바뀜
           className='my-4 rounded-2xl bg-gray-200 p-4 whitespace-pre-line'>
-          저는 어릴적부터~~는 어릴적부터~~는 어릴적부터~~는 어릴적부터~~는 어릴적부터~~는
-          어릴적부터~~는 어릴적부터~~는 어릴적부터~~는 어릴적부터~~
+          {user?.introduction || '자기소개를 입력해주세요!'}
         </p>
       </div>
 
       <Button className='w-30 self-center'>
-        <Link to={`/profile/${userId}/update`}>수정하기</Link>
+        <Link to={`/profile/${user?.id}/update`}>수정하기</Link>
       </Button>
     </div>
   );
