3d9b17bfdcb331c0b48c3f0a2d2b9a028b00619d
Dessert99 <lsjh1234@naver.com>
2025-11-20 22:11:39 +0900

    feat(profile): 프로필 관련 Custom Hooks 구현
    - useGetMe: 전역 상태 활용을 위한 캐싱 옵션 적용 (staleTime: 1h)
    - useUpdateProfile: 프로필 수정 후 쿼리 무효화(invalidateQueries) 로직 추가
    - useGetOtherProfile: 타인 프로필 조회 훅 추가

---
+++ src/hooks/queries/useProfile.ts
@@ -0,0 +1,47 @@
+import { getMe, getOtherProfile, updateProfile } from '@/apis/profile';
+import { queryClient } from '@/apis/queryClient';
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import type { UseMutationCallback } from '@/types';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export function useGetMe() {
+  return useQuery({
+    queryFn: getMe,
+    queryKey: QUERY_KEYS.auth.me(),
+    staleTime: 1000 * 60 * 60, // 1시간 동안 캐시 데이터 사용 (전역 상태 효과)
+    gcTime: 1000 * 60 * 60 * 24,
+    refetchOnWindowFocus: false, // 탭 전환할 때마다 재요청 안 함
+  });
+}
+
+export function useUpdateProfile(callbacks?: UseMutationCallback) {
+  return useMutation({
+    //mutationFn은 인자 하나(variables)만 받는 함수여야 한다
+    mutationFn: updateProfile,
+    onSuccess: () => {
+      //여기서 공통 콜백
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.auth.me(),
+      });
+      if (callbacks?.onSuccess) {
+        // 외부에서 커스텀 콜백
+        callbacks.onSuccess();
+      }
+    },
+    onError: (error: Error) => {
+      //여기서 공통 콜백
+      if (callbacks?.onError) {
+        // 외부에서 커스텀 콜백
+        callbacks.onError(error);
+      }
+    },
+  });
+}
+
+//TODO: 나중에 카드에다가 연결
+export function useGetOtherProfile(userId: number) {
+  return useQuery({
+    queryFn: () => getOtherProfile(userId),
+    queryKey: QUERY_KEYS.auth.userProfile(userId),
+  });
+}
