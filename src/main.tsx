fbd65ab7d219897c96dd9d3aa7924a5e9aeeb8eb
Dessert99 <lsjh1234@naver.com>
2025-11-21 12:51:03 +0900

    chore: StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
@@ -7,10 +6,8 @@ import { queryClient } from '@/apis/queryClient.ts';
 import { Toaster } from '@/components/ui/sonner';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <Toaster />
+  </QueryClientProvider>
 );
