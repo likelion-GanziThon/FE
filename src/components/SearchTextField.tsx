d96cb7d2bce3585d0842d622e4e5cc6a8bb3a863
Dessert99 <lsjh1234@naver.com>
2025-11-21 16:09:34 +0900

    feat: 검색 필드 필수 옵션 추가

--- src/components/SearchTextField.tsx
+++ src/components/SearchTextField.tsx
@@ -1,24 +1,29 @@
 import { Search } from 'lucide-react';
-import { Field } from './ui/field';
+import { Field, FieldError } from './ui/field';
 import { InputGroup, InputGroupAddon, InputGroupInput } from './ui/input-group';
 import { useFormContext } from 'react-hook-form';
 import type { CommunityFormValues } from '@/pages/CommunityPage';
 
 export default function SearchTextField() {
-  const { register } = useFormContext<CommunityFormValues>();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<CommunityFormValues>();
 
   return (
-    <Field>
+    <Field data-inavlid={!!errors.searchText}>
       <InputGroup>
         <InputGroupInput
+          aria-invalid={!!errors.searchText}
           placeholder='검색어를 입력해주세요.'
           type='text'
-          {...register('searchText')}
+          {...register('searchText', { required: '검색어를 입력해주세요.' })}
         />
         <InputGroupAddon align={'inline-end'}>
           <Search />
         </InputGroupAddon>
       </InputGroup>
+      {!!errors.searchText && <FieldError errors={[errors.searchText]} />}
     </Field>
   );
 }
